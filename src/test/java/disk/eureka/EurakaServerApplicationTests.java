a0a7d369b532f1f754c5b977cf0448502d9218ed
guangming_li <guangming_li@sui.com>
2020-08-17 17:54:34 +0800

    第一次提交】

---
+++ src/test/java/disk/eureka/EurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package disk.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
